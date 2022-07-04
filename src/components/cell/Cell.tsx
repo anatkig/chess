11fc4cbbaf99cadac8ec713f7468dddccf02ca12
anatkig <keeggooggle@gmail.com>
2022-07-04 13:44:43 +0600

    little fix

---
+++ src/components/cell/Cell.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Cell = () => {
+    return (
+        <div id="container"></div>
+    )
+}
+
+export default Cell;
---
+++ src/components/piece/Piece.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Piece = () => {
+    return (
+        <div id="container"></div>
+    )
+}
+
+export default Piece;
