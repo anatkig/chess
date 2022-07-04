0a1f064fa3c36b46c06b8850700608203c42bf90
anatkig <keeggooggle@gmail.com>
2022-07-04 23:10:42 +0600

    added numbers and letters to the board

--- src/components/game/container/Container.tsx
+++ src/components/game/container/Container.tsx
@@ -9,8 +9,24 @@ import './container.css';
 const Container = ({ cells }: { cells: [CellType[]] }) => {
 
     return (
-        <div className="flex container flex-wrap mx-auto my-4 border-2 border-black place-content-around">
-            {cells.map(row => row.map((cell, index) => <Cell color={cell.backgroundColor} key={index + cell.backgroundColor} />))}
+        <div className="container">
+            <div className="flex mx-auto letters">
+                {[...Array(8)].map((_, index) => <div className="text-center mx-px letter">{String.fromCharCode(97 + index)}</div>)}
+            </div>
+            <div className="flex mx-auto container-row">
+                <div className="flex flex-col place-content-around numbers">
+                    {[...Array(8)].map((_, index) => <div className="text-center flex place-content-center items-center mx-px number">{index + 1}</div>)}
+                </div>
+                <div className="flex inner-container flex-wrap mx-auto border-2 border-black place-content-around">
+                    {cells.map(row => row.map((cell, index) => <Cell color={cell.backgroundColor} key={index + cell.backgroundColor} />))}
+                </div>
+                <div className="flex flex-col  place-content-around numbers">
+                    {[...Array(8)].map((_, index) => <div className="text-center flex place-content-center items-center mx-px number">{index + 1}</div>)}
+                </div>
+            </div>
+            <div className="flex mx-auto letters">
+                {[...Array(8)].map((_, index) => <div className="text-center mx-px letter">{String.fromCharCode(97 + index)}</div>)}
+            </div>
         </div>
     )
 }
