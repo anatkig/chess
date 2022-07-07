401f926f11e416b32c79b477328a41533a87197e
anatkig <keeggooggle@gmail.com>
2022-07-07 21:19:17 +0600

    fixed rook moves

--- src/logic/allowPieceMoves.ts
+++ src/logic/allowPieceMoves.ts
@@ -71,8 +71,45 @@ const allowPieceMoves = (
         return true;
       }
     } else if (initType === "rook") {
-      if (initRow === rowIndex || initCell === cellIndex) {
-        return true;
+      if (initRow === rowIndex) {
+        if (initCell > cellIndex) {
+          if (
+            cells[rowIndex]
+              .slice(cellIndex + 1, initCell)
+              .every((cell) => cell.child === "") &&
+            !cells[rowIndex][cellIndex].child.includes(initPieceColor)
+          ) {
+            return true;
+          }
+        } else {
+          if (
+            cells[rowIndex]
+              .slice(initCell + 1, cellIndex)
+              .every((cell) => cell.child === "") &&
+            !cells[rowIndex][cellIndex].child.includes(initPieceColor)
+          ) {
+            return true;
+          }
+        }
+      } else if (initCell === cellIndex) {
+        // the cells which the rook passes before reaching the current cell
+        const passedCells: CellType[] = [];
+
+        if (initRow < rowIndex) {
+          cells.slice(initRow + 1, rowIndex).forEach((row, rowInd) => {
+            passedCells.push(cells[rowInd + initRow + 1][cellIndex]);
+          });
+        } else {
+          cells.slice(rowIndex + 1, initRow).forEach((row, rowInd) => {
+            passedCells.push(cells[rowInd + initRow + 1][cellIndex]);
+          });
+        }
+        if (
+          passedCells.every((cell) => cell.child === "") &&
+          !cells[rowIndex][cellIndex].child.includes(initPieceColor)
+        ) {
+          return true;
+        }
       }
     } else if (initType === "bishop") {
       if (Math.abs(initRow - rowIndex) === Math.abs(initCell - cellIndex)) {
