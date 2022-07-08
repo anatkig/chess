d3aca2b4b5cb365abfc5eb91b408248f08fda361
anatkig <keeggooggle@gmail.com>
2022-07-08 08:17:14 +0600

    refactored rook logic

---
+++ src/logic/allowRookMoves.ts
@@ -0,0 +1,53 @@
+import { CellType } from "../types/types";
+
+const allowRookMoves = (
+  initRow: number,
+  rowIndex: number,
+  initCell: number,
+  cellIndex: number,
+  cells: [CellType[]],
+  initPieceColor: string
+) => {
+  if (initRow === rowIndex) {
+    if (initCell > cellIndex) {
+      if (
+        cells[rowIndex]
+          .slice(cellIndex + 1, initCell)
+          .every((cell) => cell.child === "") &&
+        !cells[rowIndex][cellIndex].child.includes(initPieceColor)
+      ) {
+        return true;
+      }
+    } else {
+      if (
+        cells[rowIndex]
+          .slice(initCell + 1, cellIndex)
+          .every((cell) => cell.child === "") &&
+        !cells[rowIndex][cellIndex].child.includes(initPieceColor)
+      ) {
+        return true;
+      }
+    }
+  } else if (initCell === cellIndex) {
+    // the cells which the rook passes before reaching the current cell
+    const passedCells: CellType[] = [];
+
+    if (initRow < rowIndex) {
+      cells.slice(initRow + 1, rowIndex).forEach((row, rowInd) => {
+        passedCells.push(cells[rowInd + initRow + 1][cellIndex]);
+      });
+    } else {
+      cells.slice(rowIndex + 1, initRow).forEach((row, rowInd) => {
+        passedCells.push(cells[rowInd + initRow + 1][cellIndex]);
+      });
+    }
+    if (
+      passedCells.every((cell) => cell.child === "") &&
+      !cells[rowIndex][cellIndex].child.includes(initPieceColor)
+    ) {
+      return true;
+    }
+  }
+};
+
+export default allowRookMoves;
